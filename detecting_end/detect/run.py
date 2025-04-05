78221a7b15b91272c0fc0e57f1a62d0055ea5f33
acer <1742782025@qq.com>
2025-04-06 01:47:08 +0800

    检测端修复2

--- detecting_end/detect/run.py
+++ detecting_end/detect/run.py
@@ -2,6 +2,9 @@ from ultralytics import YOLO
 import os
 import torch
 
+from ultralytics.nn.modules import C3
+torch.serialization.add_safe_globals([C3])
+
 def detect(file):
     model_path = os.path.join(os.path.dirname(__file__), "detect_model.pt")
     model = torch.load(model_path, weights_only=False)
